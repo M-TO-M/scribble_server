b78a635c053288a71f214d3205e3ae4cab03b5d0
soheeeeP <tpfj@naver.com>
2022-04-26 22:46:00 +0900

    IMPLEMENT #1 - 도서 ISBN 저장을 위한 custom field, validator

--- utils/validators.py
+++ utils/validators.py
@@ -1,8 +1,25 @@
+import re
+from stdnum import isbn
+
 from django.core.validators import EmailValidator
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
 domain_allowlist = ["naver.com", "daum.com", "gmail.com", "icloud.com"]
+
+
+def ISBNValidator(input_isbn):
+    normalized = re.sub(" |-", "", input_isbn).upper()
+
+    if not isinstance(normalized, str):
+        raise ValidationError(_(u'invalid ISBN: not a string'))
+    if len(normalized) != 10 or len(normalized) != 13:
+        raise ValidationError(_(u'invalid ISBN: wrong length'))
+    if not isbn.is_valid(normalized):
+        raise ValidationError(_(u'invalid ISBN: failed checksum'))
+
+    return True
 
 
 class SpecificEmailDomainValidator(EmailValidator):
