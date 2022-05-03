3f7c4c9404ceadbcfdd8d4328440850eece7840a
soheeeeP <tpfj@naver.com>
2022-05-03 13:05:06 +0900

    ADD #2 - url 연결

---
+++ api/contents/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from api.contents.views import *
+
+
+app_name = 'contents'
+
+urlpatterns = [
+
+]
---
+++ api/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from api.users.views import *
+
+
+app_name = 'users'
+
+urlpatterns = [
+
+]
