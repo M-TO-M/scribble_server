d06dd688041de4a5719042772318562b4bf4df38
soheeeeP <tpfj@naver.com>
2022-05-04 23:17:53 +0900

    ADD #9 - get_object() custom 404 exception

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,3 +1,7 @@
+from django.utils.translation import gettext_lazy as _
+
+from rest_framework import status
+from rest_framework.exceptions import APIException
 from rest_framework.views import exception_handler
 
 
@@ -19,3 +23,9 @@ def custom_exception_handler(exc, context):
 
     response.data = _data
     return response
+
+
+class UserNotFound(APIException):
+    status_code = status.HTTP_404_NOT_FOUND
+    default_detail = _('no_exist_user')
+    default_code = 'not_found'
