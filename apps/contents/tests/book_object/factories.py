c343443663fd2084da68c70270ac7b1065cf7096
soheeeeP <tpfj@naver.com>
2022-05-08 21:30:24 +0900

    ADD & IMPLEMENT #23 #24 - BookObject Fake Factory 생성 및 book endpoint test 작성

---
+++ apps/contents/tests/book_object/factories.py
@@ -0,0 +1,31 @@
+import uuid
+import random
+import base64
+import factory
+from faker import Faker
+from faker.providers.isbn import Provider
+
+from apps.users.models import category_choices
+from apps.contents.models import BookObject
+
+
+class BookObjectFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = BookObject
+        django_get_or_create = ['isbn']
+
+    isbn = factory.LazyAttribute(lambda n: Provider(generator=Faker()).isbn13(separator=''))
+    title = factory.Sequence(lambda n: f'title_{n}')
+    author = factory.Sequence(lambda n: f'author_{n}')
+    publisher = factory.Sequence(lambda n: f'publisher_{n}')
+    thumbnail = factory.LazyAttribute(lambda n: base64.urlsafe_b64encode(uuid.uuid4().bytes))
+
+    @factory.lazy_attribute_sequence
+    def category(self, n):
+        value = {}
+        category_dict = {i: value for i, value in enumerate(category_choices)}
+
+        for i in range(1, (n + 1) % len(category_dict)):
+            idx = random.randrange(i)
+            value[idx] = category_dict[idx]
+        return value
