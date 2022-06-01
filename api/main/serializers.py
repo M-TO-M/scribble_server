fc4c3abadb1a5b56adb07695aa02a1ceda3143ce
soheeeeP <tpfj@naver.com>
2022-06-01 16:03:50 +0900

    ADD #36 - schema용 serializer 생성

---
+++ api/main/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+
+from api.contents.note.serializers import NoteSchemaSerializer
+from api.contents.page.serializers import PageSchemaSerializer
+
+
+class MainSchemaSerializer(serializers.Serializer):
+    count = serializers.IntegerField(help_text='전체 게시물(페이지) 수', read_only=True)
+    previous_offset = serializers.IntegerField(help_text='조회 가능한 이전 start offset', read_only=True)
+    next_offset = serializers.IntegerField(help_text='조회 가능한 다음 offset', read_only=True)
+    pages = PageSchemaSerializer(many=True, help_text='페이지', read_only=True)
+
+
+class UserMainSchemaSerializer(serializers.Serializer):
+    count = serializers.IntegerField(help_text='전체 게시물(페이지) 수', read_only=True)
+    previous_offset = serializers.IntegerField(help_text='조회 가능한 이전 start offset', read_only=True)
+    next_offset = serializers.IntegerField(help_text='조회 가능한 다음 offset', read_only=True)
+    notes = NoteSchemaSerializer(many=True, help_text='노트', read_only=True)
+
