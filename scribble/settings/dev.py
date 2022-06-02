1c577adea27fcf663cce3848bb7ce9cba172b929
soheeeeP <tpfj@naver.com>
2022-06-02 20:00:04 +0900

    FIX - redis location set bugs

--- scribble/settings/dev.py
+++ scribble/settings/dev.py
@@ -11,7 +11,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://redis:6379",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
