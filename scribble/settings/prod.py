0a219cfa5d53597c7e28c985f6694bc05e32b801
soheeeep <tpfj@naver.com>
2022-08-16 17:58:26 +0900

    update STATIC_ROOT in prod

--- scribble/settings/prod.py
+++ scribble/settings/prod.py
@@ -38,7 +38,7 @@ CACHES = {
 }
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR.parent, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR.parent, 'app/static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR.parent, 'media')
